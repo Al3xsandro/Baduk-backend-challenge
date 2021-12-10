f3e00cf08d61a86fd016524081953232f652e5c0
Al3xsandro <alexs123655@gmail.com>
2021-12-10 20:25:18 -0300

    feat: add swagger open api

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,9 +3,11 @@ import "dotenv/config";
 
 import cors from "cors";
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import "express-async-errors";
 
+import swaggerFile from "../../../swagger.json";
 import createConnection from "../typeorm/index";
 import { handleErrors } from "./middlewares/handleErrors";
 import { router } from "./routes";
@@ -14,6 +16,7 @@ import "../../container/index";
 
 createConnection();
 const app = express();
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(cors());
 app.use(express.json());
