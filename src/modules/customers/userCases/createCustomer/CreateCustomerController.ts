0741403a6ac51484371156552700c49adc5d154a
Al3xsandro <alexs123655@gmail.com>
2021-12-10 20:18:37 -0300

    feat: validate body request

--- src/modules/customers/userCases/createCustomer/CreateCustomerController.ts
+++ src/modules/customers/userCases/createCustomer/CreateCustomerController.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+import validator from "validator";
 
+import { AppError } from "../../../../shared/errors/AppError";
 import { ICreateCustumerDTO } from "../../dtos/ICreateCustomer.dto";
 import { CreateCustomerUseCase } from "./CreateCustomerUseCase";
 
 class CreateCustomerController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, email, telefone }: ICreateCustumerDTO = request.body;
+
+    if (!validator.isEmail(email)) {
+      throw new AppError("Invalid e-mail");
+    }
+
+    if (!validator.isMobilePhone(telefone)) {
+      throw new AppError("Invalid phone number");
+    }
 
     const createCustomerUseCase = container.resolve(CreateCustomerUseCase);
 
