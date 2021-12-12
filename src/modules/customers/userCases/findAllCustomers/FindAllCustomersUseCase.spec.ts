ed576cbd68b2616546447e5a65166720e1512994
Al3xsandro <alexs123655@gmail.com>
2021-12-12 13:20:57 -0300

    chore: change customer useCase test

--- src/modules/customers/userCases/findAllCustomers/FindAllCustomersUseCase.spec.ts
+++ src/modules/customers/userCases/findAllCustomers/FindAllCustomersUseCase.spec.ts
@@ -1,7 +1,6 @@
 import { ICustomerRepository } from "@modules/customers/repositories/ICustumerRepository";
 import { CustomerRepositoryInMemory } from "@modules/customers/repositories/implementations/in-memory/CustomerRepositoryInMemory";
 import { CreateCustomerUseCase } from "@modules/customers/userCases/createCustomer/CreateCustomerUseCase";
-import { AppError } from "@shared/errors/AppError";
 
 describe("Customers", () => {
   let customerRepositoryInMemory: ICustomerRepository;
@@ -15,31 +14,18 @@ describe("Customers", () => {
     expect(customerUserCase).toBeDefined();
   });
 
-  describe("[POST] Create custumer", () => {
-    it("should be able to create a new customer", async () => {
-      const customer = await customerRepositoryInMemory.create({
+  describe("[GET] Get all customers", () => {
+    it("should be able to receive all customers created", async () => {
+      await customerRepositoryInMemory.create({
         name: "john",
         email: "john@mock.com",
         telefone: "11945058392",
       });
 
-      expect(customer).toHaveProperty("id");
-      expect(customer).toEqual({
-        id: customer.id,
-        name: "john",
-        email: "john@mock.com",
-        telefone: customer.telefone,
-      });
-    });
+      const customers = await customerRepositoryInMemory.find();
 
-    it("should receive an error on create a new customer with an existing email", async () => {
-      await expect(
-        customerUserCase.execute({
-          name: "john",
-          email: "john@mock.com",
-          telefone: "11945058392",
-        })
-      ).rejects.toEqual(new AppError("Customer already exists!"));
+      expect(customers).toEqual(customers);
+      expect(customers[0]).toHaveProperty("id");
     });
   });
 });
