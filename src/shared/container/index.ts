209f40a1cb4c728a83b7e5fc314bc38b37c08a22
Al3xsandro <alexs123655@gmail.com>
2021-12-12 14:00:43 -0300

    chore: add products repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,17 @@
 import { container } from "tsyringe";
 
+import { ProductsRepository } from "@modules/products/infra/typeorm/repositories/implementations/ProductsRepository";
+import { IProductsRepository } from "@modules/products/repositories/IProductsRepository";
+
 import { CustumerRepository } from "../../modules/customers/infra/typeorm/repositories/implementations/CustomerRepository";
 import { ICustomerRepository } from "../../modules/customers/repositories/ICustumerRepository";
 
 container.registerSingleton<ICustomerRepository>(
   "CustomerRepository",
   CustumerRepository
+);
+
+container.registerSingleton<IProductsRepository>(
+  "ProductsRepository",
+  ProductsRepository
 );
